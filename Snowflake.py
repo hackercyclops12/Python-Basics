91c99faa0e23b0113cbdbca7d1fbe637ce0be851
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-04-21 11:56:37 +1200

    Add files via upload

---
+++ Snowflake.py
@@ -0,0 +1,15 @@
+from turtle import *
+import random
+COL = random.choice(['red','orange','yellow','green','blue','magenta'])
+COL2 = random.choice(['red','orange','yellow','green','blue','magenta'])
+color(COL)
+bgcolor(COL2)
+for i in range (10):
+    for i in range (2):
+        forward(100)
+        right(60)
+        forward(100)
+        right(120)
+    right(36)
+    
+
