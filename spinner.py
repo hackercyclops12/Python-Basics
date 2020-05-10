a95e9bd4633d3736bdaf0ae9c627045a66589618
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-05-11 11:17:49 +1200

    Add files via upload

---
+++ spinner.py
@@ -0,0 +1,30 @@
+from turtle import *
+speed('fastest')
+v = 66
+pensize(150)
+bgcolor('grey')
+while True:
+    v = v + 0.5
+    right(90)
+    color('red')
+    forward(v)
+    rt(90)
+    color('orange')
+    forward(v)
+    left(90)
+    color('yellow')
+    forward(v)
+    lt(v)
+    color('green')
+    forward(v)
+    right(90)
+    color('cyan')
+    forward(v)
+    rt(90)
+    color('blue')
+    forward(v)
+    left(90)
+    color('purple')
+    forward(v) 
+    undo()
+              
