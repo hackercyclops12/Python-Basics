b82ae995c17675dc4750c78b6b4a8581f57882fa
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-08-19 11:15:00 +1200

    Add files via upload

---
+++ Txt2Bin.py
@@ -0,0 +1,46 @@
+import pygame
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+
+print(">>>>>>>>>>Text to Binary Convertor<<<<<<<<<<\n")
+
+conversion=input("Enter text to convert to Binary: ")
+
+
+size = (1920, 1080)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Binary Conversion")
+
+done = False
+clock = pygame.time.Clock()
+ 
+text_rotate_degrees = 0
+
+Binary=(' '.join(format(ord(x), 'b') for x in conversion))
+
+while not done:
+ 
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:  
+            done = True  
+
+    screen.fill(WHITE)
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    
+    text = font.render(Binary, True, BLACK)
+    text = pygame.transform.rotate(text, text_rotate_degrees)
+    screen.blit(text, [100, 500])
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
+
+print(' '.join(format(ord(x), 'b') for x in conversion))
