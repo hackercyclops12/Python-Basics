21c36be1f703473f2b82aff1c9f168be6618c19b
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-11 07:45:27 +1300

    Added comments and renamed file
    Added comments to help beginners understand this script better 
    
    And also gave this file a name that actually suited it

---
+++ AUTObiography.py
@@ -0,0 +1,13 @@
+#AUTObiography - Questionare
+
+#ask users for inputs
+Name = input("What is your name? ")
+Age = input("How old are you? ")
+Gender = input("Are you a He or a She? ")
+Location = input("where are you? ")
+Company =input("who is with you? ")
+
+#relay information back to the users console
+print('This is ' + Name) 
+print(Gender + " is " + Age + ' years old') 
+print(Gender + ' is at ' + Location +", "+ Company + " is with him/her")
