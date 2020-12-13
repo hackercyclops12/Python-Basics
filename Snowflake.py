9cd6aca3fff9527187a8e630466f1f0c2482f91e
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-14 10:24:16 +1300

    Added comments
    Added comments to help beginners to further understand this script

--- Snowflake.py
+++ Snowflake.py
@@ -1,9 +1,16 @@
+#import functions
 from turtle import *
 import random
+
+#define varable + pick a color and asign it to said variable
 COL = random.choice(['red','orange','yellow','green','blue','magenta'])
 COL2 = random.choice(['red','orange','yellow','green','blue','magenta'])
+
+#set turtle color + window background color 
 color(COL)
 bgcolor(COL2)
+
+#mainloop
 for i in range (10):
     for i in range (2):
         forward(100)
