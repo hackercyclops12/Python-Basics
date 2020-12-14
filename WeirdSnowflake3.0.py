ace6208f150540b4496eebb4936da20f250bc509
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-14 16:26:11 +1300

    Added comments
    Added comments to help beginners to understand this script

--- WeirdSnowflake3.0.py
+++ WeirdSnowflake3.0.py
@@ -1,11 +1,18 @@
+#import functions
 from turtle import *
 import random
+
+#print to console + set turtle mode + set variable
 print("The longer you wait the better this looks!")
 speed("fastest")
 pensize(20)
 N = 1
+
+#define varible + choose color to assign to said variable 
 COL = random.choice(['red','orange','yellow','green','blue','magenta'])
 bgcolor("grey")
+
+#mainloop
 while True:
     N = N + 2.5
     for i in range (2):
@@ -16,7 +23,5 @@ while True:
         forward(N)
         right(150)
     right(222)
-
     
     
-
