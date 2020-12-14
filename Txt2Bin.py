71ee753b312566bfd3745e2b1a282aa19b49ba66
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-14 16:02:26 +1300

    Added comments
    Added comments to help beginners to fully understand this script

--- Txt2Bin.py
+++ Txt2Bin.py
@@ -1,32 +1,36 @@
+#import pygame and set it up
 import pygame
 pygame.init()
 
+#set variables 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
  
-
+#print header to console
 print(">>>>>>>>>>Text to Binary Convertor<<<<<<<<<<\n")
 
+#ask use for what to convert to binary
 conversion=input("Enter text to convert to Binary: ")
 
-
+#create variable + draw pygame window (1920/1080)
 size = (1920, 1080)
 screen = pygame.display.set_mode(size)
- 
+
+#write text into said pygame window
 pygame.display.set_caption("Binary Conversion")
 
+#create variable
 done = False
 clock = pygame.time.Clock()
- 
 text_rotate_degrees = 0
 
 Binary=(' '.join(format(ord(x), 'b') for x in conversion))
 
-while not done:
- 
+#mainloop
+while not done: 
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
             done = True  
