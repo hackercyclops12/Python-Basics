69b4a666588b180849095a43ceba52b54083830d
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-14 16:21:45 +1300

    Added comments
    Added comments to help beginners to fully understand this script

--- WeirdSnowflake.py
+++ WeirdSnowflake.py
@@ -1,12 +1,21 @@
+#import functions
 from turtle import *
 import random
+
+#define variables + assign a color to said variable
 COL = random.choice(['red','orange','yellow','green','blue','magenta'])
 COL2 = random.choice(['red','orange','yellow','green','blue','magenta'])
+
+#check if both colors picked are the same, if so change them 
 if COL == COL2:
     COL = random.choice(['red','orange','yellow','green','blue','magenta'])
 else:
     color(COL)
+    
+#set background of turtle window
 bgcolor(COL2)
+
+#mainloop
 for i in range (10):
     for i in range (2):
         forward(100)
